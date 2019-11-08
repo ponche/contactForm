527a5f50f61e44a8740ee505fdd27cb82c1595e5
Rémi <ponche62880@gmail.com>
2019-11-08 14:20:49 +0100

    create the fixture class

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,7 +2,6 @@
 
 namespace App\DataFixtures;
 
-use App\Entity\Department;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
@@ -13,28 +12,5 @@ class AppFixtures extends Fixture
         // $product = new Product();
         // $manager->persist($product);
 
-        $devDepartment = new Department(); 
-        $devDepartment->setName('Dev'); 
-        $devDepartment->setMail('ponche62880@gmail.com'); 
-        $manager->persist($devDepartment); 
-
-        $rhDepartment = new Department(); 
-        $rhDepartment->setName('RH'); 
-        $rhDepartment->setMail('ponche62880@gmail.com'); 
-        $manager->persist($rhDepartment); 
-
-        $directionDepartment = new Department(); 
-        $directionDepartment->setName('Direction'); 
-        $directionDepartment->setMail('ponche62880@gmail.com'); 
-        $manager->persist($directionDepartment); 
-
-        $comDepartment = new Department(); 
-        $comDepartment->setName('Communication'); 
-        $comDepartment->setMail('ponche62880@gmail.com'); 
-        $manager->persist($comDepartment); 
-
-
-
-        $manager->flush();
     }
 }
