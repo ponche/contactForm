235eb9682a2ec02f0cd275e27be4052202d1aaa0
Rémi <ponche62880@gmail.com>
2019-11-14 14:57:28 +0100

    add the serializer annnotation on the entity
    add the annotation to hidden the mail and contact fields

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -37,9 +37,9 @@ class ApiController extends AbstractFOSRestController
     public function postContactsAction(Request $request, MailManager $mailManager)
     {
 
-        //code copier de Daishi
-
         $contact = new Contact();
+        $view = View::create();
+        $view->setFormat('json');
         $form = $this->createForm(ContactType::class, $contact, [
             'csrf_protection' => false,
         ]);
@@ -56,46 +56,14 @@ class ApiController extends AbstractFOSRestController
 
 
             $view = $this->view($contact, Response::HTTP_CREATED);
-
+            $view->setFormat('json');
             return $this->handleView($view);
         }
 
         $view = $this->view($form, Response::HTTP_BAD_REQUEST);
-
+        $view->setFormat('json');
         return $this->handleView($view);
-        // $contact = new Contact(); 
-
-        // $form = 
-        // // Fictive; 
-        // // recupere le departement dev ( id=1 ) pour le test 
-        // $department = $this->departmentRepository->find(1);
-
-        // // creation d'un contact test 
-        // $contact
-        //     ->setFirstname("Rémi")
-        //     ->setLastname("PONCHE")
-        //     ->setMail("ponche62880@gmail.com")
-        //     ->setDepartment($department)
-        //     ->setMessage("Envoyé via une requete API")
-        //     ->setCreateAt(new \DateTime())
-        //     ;
-
-        // // sauvegarde dans la BDD 
-        // $em = $this->getDoctrine()->getManager(); 
-        // $em->persist($contact);
-        // $em->flush(); 
-
-
-        // vérifie les data  si erreur lever une excepetion 
-        // return (Exception )
-
-        // $mailManager->sendMailToDepartment($contact); 
-
-        // $dataTest = ["is Good"];
-        // $view = View::create($dataTest, 200); 
-        // $view->setFormat('json'); 
-
-        // return $this->handleView($view);        
+        
     }
 
     /**
@@ -104,55 +72,20 @@ class ApiController extends AbstractFOSRestController
     public function getDepartmentsAction()
     {
         $listDepartment = $this->departmentRepository->findAll(); 
-        dump($listDepartment); 
-        $dataTest = ['toto', 'titi']; 
-        $view = View::create($dataTest, 200);
+        $view = View::create($listDepartment, Response::HTTP_OK);
         $view->setFormat('json'); 
-        dump($view); 
-
-        // l'envoi marche faudrau juste filtrer 
-
-        // $view = $this->view($listDepartment, 200); 
-
-        // supprimer les mail et les relations dans listDepartment
-
-        // envoi les data à la vue  
 
         return $this->handleView($view);
 
     }
-
     /**
-     * @Route("/test")
+     * @Get("/department/{id}")
      */
-    public function test(DepartmentRepository $departmentRepository)
-    {
-        $listDepartment = $departmentRepository->findAll(); 
-        dump($listDepartment); 
-
-        return $this->render('contact/notification.html.twig');
-    }
-
-
-    public function postPromotionAction(Request $request)
-    {
-        $promotion = new Promotion();
-        $form = $this->createForm(PromotionType::class, $promotion, [
-            'csrf_protection' => false,
-        ]);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($promotion);
-            $entityManager->flush();
-
-            $view = $this->view($promotion, Response::HTTP_CREATED);
-
-            return $this->handleView($view);
-        }
-
-        $view = $this->view($form, Response::HTTP_BAD_REQUEST);
+    public function getDepartmentAction($id)
+    {  
+        $listDepartment = $this->departmentRepository->find($id); 
+        $view = View::create($listDepartment, Response::HTTP_OK);
+        $view->setFormat('json'); 
 
         return $this->handleView($view);
     }
--- src/Entity/Department.php
+++ src/Entity/Department.php
@@ -5,9 +5,12 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use JMS\Serializer\Annotation as Serializer;
+use JMS\Serializer\SerializerInterface;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\DepartmentRepository")
+ * @Serializer\ExclusionPolicy("ALL")
  */
 class Department
 {
@@ -15,11 +18,13 @@ class Department
      * @ORM\Id()
      * @ORM\GeneratedValue()
      * @ORM\Column(type="integer")
+     * @Serializer\Expose
      */
     private $id;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Serializer\Expose
      */
     private $name;
 
